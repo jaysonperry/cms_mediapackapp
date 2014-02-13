3429b12fdd9a77d32140263e0e5e948a4d1805f6
Jayson Perry <jayson.t.perry@gmail.com>
2014-02-13 11:18:55 -0800

    commit all - BrowserCMS

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CmsDemo::Application.config.secret_token = '38e852f3271a26e9e9e1d251f68fd7db0b86896c1edfcb9bb066a33842fd28581f485294795fecebad8a65e9f6b83d470eb4a44c48ac5e9bdda60a3cbeb30bf9'
